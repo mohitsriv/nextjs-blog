ab1da8bf5c694149089c2f94a4977a291a3c783e
Mohit Srivastava <mohitsriv@hotmail.com>
2021-12-14 09:12:53 -0800

    SEcond

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Mohit Srivastava'
+const name = 'Mohit Srivastava the first'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
